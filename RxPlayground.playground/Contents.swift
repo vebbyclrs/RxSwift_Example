becda6fa0c9ec9f3d90af109e6755c1b0babf123
Wowo Diergo Suciawo <wowo.suciawo@tokopedia.com>
2019-11-05 09:04:11 +0700

    Add empty playground

--- RxPlayground.playground/Contents.swift
+++ RxPlayground.playground/Contents.swift
@@ -1,19 +1,5 @@
-import UIKit
 import PlaygroundSupport
-import RxCocoa
+PlaygroundPage.current.needsIndefiniteExecution = true
 import RxSwift
 
-var str = "Hello, playground"
-let signUpButton = UIButton()
-signUpButton.setTitle("Tap me!", for: .normal)
-signUpButton.sizeToFit()
-
-let didTapSignUp = PublishSubject<Void>()
-
-signUpButton.rx.tap.subscribe(didTapSignUp)
-
-didTapSignUp.subscribe { _ in
-    print("I got tapped!")
-}
-
-PlaygroundPage.current.liveView = signUpButton
+let number = Observable<String>.of("satu")
